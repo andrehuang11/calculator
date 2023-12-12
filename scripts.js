f191498613fc67302e6f4fe45db834edfb653233
Andre <andre.huang2010@gmail.com>
2023-12-12 20:40:33 -0300

    Add function to display values pressed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let display_value;
+
 function sum(a , b) {
     return a + b;
 };
@@ -19,3 +21,20 @@ let first, second, operator;
 function operate(first, second, operator) {
     operator(first, second);
 };
+
+function digit_pressed(digit) {
+    if (document.getElementById("output").innerHTML == "0") {
+        document.getElementById("output").innerHTML = null;
+    } if (digit == "C") {
+        document.getElementById("output").innerHTML = "0";
+    } else {
+        document.getElementById("output").innerHTML += digit;
+    };
+};
+
+document.querySelectorAll('button').forEach(el =>{
+    el.addEventListener('click', () =>{
+        digit_pressed(el.textContent);
+        display_value = el.textContent;
+    });
+});
